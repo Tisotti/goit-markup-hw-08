53029bd82179606c65e70f0a56f50fd5d8dbc551
Tisotti <97412592+Tisotti@users.noreply.github.com>
2022-04-20 00:51:01 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+(() => {
+    const newLocal = "[data-modal]";
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector(newLocal),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
